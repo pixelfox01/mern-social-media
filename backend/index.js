5c0c7f9e7a567e36d193420cf911cb67f01a1e78
Imran Yafith <imranyafith@gmail.com>
2024-01-03 14:19:08 -0500

    Removed unnecessary webhook endpoint and updated default port

--- backend/index.js
+++ backend/index.js
@@ -3,56 +3,22 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import mongoose from "mongoose";
 import cors from "cors";
-import { Webhook } from "svix";
-import bodyParser from "body-parser";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.DB_URL;
 
 const app = express();
 app.use(cors());
 
-app.post(
-  "/api/webhook",
-  bodyParser.raw({ type: "application/json" }),
-  async (req, res) => {
-    console.log("Webhook received");
-    try {
-      const payloadString = req.body.toString();
-      const svixHeaders = req.headers;
-
-      const wh = new Webhook(process.env.WEBHOOK_SECRET);
-      const evt = wh.verify(payloadString, svixHeaders);
-      const { id, ...attributes } = evt.data;
-
-      // Handle the webhooks
-      const eventType = evt.type;
-      if (eventType === "user.created") {
-        console.log(`User ${id} was ${eventType}`);
-        console.log(attributes);
-      }
-      res.status(200).json({
-        success: true,
-        message: "Webhook received",
-      });
-    } catch (err) {
-      res.status(400).json({
-        success: false,
-        message: err.message,
-      });
-    }
-  }
+app.get("/", (req, res) =>
+  res.status(200).send("Welcome to MERN social media app")
 );
 
-// app.use(express.json());
-
-app.get("/", (req, res) => {
-  return res.status(200).send("Welcome to MERN social media app");
-});
-
 app.use("/users", userRoutes);
+
+// app.use(express.json());
 
 mongoose
   .connect(DB_URL)
